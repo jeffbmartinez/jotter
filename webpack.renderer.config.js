79eaec6ee1cbcf6a32042c356a3e358fd9fbe288
Jeff Martinez <jeffbmartinez@gmail.com>
2021-04-03 22:18:16 -0700

    Restarting app with electron-forge, webpack, and others

---
+++ webpack.renderer.config.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const rules = require('./webpack.rules');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugins = require('./webpack.plugins');
+
+rules.push(
+  {
+    test: /\.css$/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: 'css-loader' },
+    ],
+  },
+  {
+    test: /\.(scss)$/,
+    use: [
+      { loader: 'style-loader' },
+      { loader: 'css-loader' },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: function () {
+              return [
+                require('autoprefixer'),
+              ];
+            },
+          },
+        },
+      },
+      { loader: 'sass-loader' },
+    ],
+  },
+  {
+    test: /\.(png|jpe?g|gif|ico|svg)$/,
+    use: [
+      { loader: 'file-loader' },
+    ],
+  },
+);
+
+module.exports = {
+  module: {
+    rules,
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.scss', '.css'],
+  },
+};
